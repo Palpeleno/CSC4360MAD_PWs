cb0254e13382e7de2d84cb53ecb3399a7c6c666d
VictoriaRobles3 <traroblx@gmail.com>
2024-02-27 11:19:36 -0500

    Add animation to Welcome Screen.

--- katoria_jmt/lib/features/app/welcome_screen/welcomeScreen.dart
+++ katoria_jmt/lib/features/app/welcome_screen/welcomeScreen.dart
@@ -1,31 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'dart:async';
+import 'package:google_fonts/google_fonts.dart';
 
-
-class WelcomeScreen extends StatefulWidget{
+class WelcomeScreen extends StatefulWidget {
   final Widget? child;
   const WelcomeScreen({super.key, this.child});
 
   @override
-  State<WelcomeScreen> createState() => _WelcomeScreen();
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreen extends State<WelcomeScreen>{
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  late String _displayText;
+  late int _index;
 
   @override
-  void initState(){
-    Future.delayed(Duration(seconds: 3),(){
-      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=> widget.child!), (route) => false);
-    }
-    );
+  void initState() {
+    _displayText = "";
+    _index = 0;
+
+    String _fullText = "Welcome to: \n Katoria \n Journal and Mood Tracker  ";
+
+    Timer.periodic(Duration(milliseconds: 75), (Timer timer) {
+      if (_index < _fullText.length) {
+        setState(() {
+          _displayText = _fullText.substring(0, _index + 1);
+          _index++;
+        });
+      } else {
+        timer.cancel();
+        _nextScreen();
+      }
+    });
     super.initState();
+  }
+
+  void _nextScreen() {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => widget.child!),
+      (route) => false,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Welcome to Katoria Jounal and Mood Tracker App", style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          textTheme: GoogleFonts.podkovaTextTheme(),
+        ),
+        home: Scaffold(
+          backgroundColor: Color.fromARGB(255, 246, 251, 245),
+          body: Center(
+            child: Text(
+              _displayText,
+              textAlign: TextAlign.center,
+              style: GoogleFonts.podkova(
+                  textStyle: const TextStyle(
+                color: Colors.black,
+                fontSize: 33,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ),
       ),
     );
