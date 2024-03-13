16f258052bdd08925b78e800928375cffe2517e1
Kush <islayusim@live.com>
2024-03-13 17:59:52 -0400

    added comments in main for db initializing depending on the build type web/andriod emulators respectivly

--- katoria_jmt/lib/main.dart
+++ katoria_jmt/lib/main.dart
@@ -22,9 +22,11 @@ import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
 void main() async {
-// initialize databse
-// Ensure that sqflite_common_ffi is properly initialized
+// initialize database
+  //intialize database for andriod development
   // WidgetsFlutterBinding.ensureInitialized();
+
+// Ensure that sqflite_common_ffi is properly initialized
   sqfliteFfiInit();
   databaseFactory = databaseFactoryFfiWeb;
 
