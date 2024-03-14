5876e6a4fd0a715b4607c6f4a20849d817de010f
Kush <islayusim@live.com>
2024-03-13 21:34:25 -0400

    jounral app bar titile them darkmode integration

--- katoria_jmt/lib/view/home/journal_entries.dart
+++ katoria_jmt/lib/view/home/journal_entries.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: sort_child_properties_last, prefer_const_literals_to_create_immutables, prefer_const_constructors, use_key_in_widget_constructors, prefer_const_constructors_in_immutables, unused_import
 
 import 'package:flutter/material.dart';
+import 'package:katoria_jmt/common/color_extension.dart';
 
 import 'package:katoria_jmt/db/page_database.dart';
 import 'package:katoria_jmt/view/home/addnewpage_view.dart';
@@ -26,7 +27,9 @@ class _JournalViewState extends State<JournalView> {
       appBar: AppBar(
         title: Text(
           "Journal Pages",
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
         ),
         centerTitle: true,
         actions: [
@@ -62,7 +65,6 @@ class _JournalViewState extends State<JournalView> {
       // ,sizedBox(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => AddPage()),
