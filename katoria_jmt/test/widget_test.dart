4a010bc91668d37a4cc42c27d5dbfb3ce76fbab9
Kush <islayusim@live.com>
2024-03-15 20:13:46 -0400

    fix test directory code

--- katoria_jmt/test/widget_test.dart
+++ katoria_jmt/test/widget_test.dart
@@ -13,11 +13,7 @@ import 'package:katoria_jmt/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-<<<<<<< HEAD
-    await tester.pumpWidget(const MyApp());
-=======
     await tester.pumpWidget(const MainApp());
->>>>>>> proj_merger
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
